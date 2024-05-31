e523f54ea28f3ea0d3b9852bd501d62ad917de9a
Ignacio Zayas <bs.zayas@gmail.com>
2024-05-31 19:05:23 -0300

    New endpoint login role Admin

--- Routes/AdminRoute.js
+++ Routes/AdminRoute.js
@@ -7,6 +7,31 @@ import path from "path";
 
 const router = express.Router();
 
+import sql from 'mssql';
+
+router.post("/adminlogin", (req, res) => {
+    const sqlQuery = "SELECT * from admin Where email = @email and password = @password";
+    
+    pool.request()
+        .input('email', sql.VarChar, req.body.email)
+        .input('password', sql.VarChar, req.body.password)
+        .query(sqlQuery, (err, result) => {
+            if (err) return res.json({ loginStatus: false, Error: "Query error" + err });
+            if (result.recordset.length > 0) {
+                const email = result.recordset[0].email;
+                const token = jwt.sign(
+                    { role: "admin", email: email, id: result.recordset[0].id },
+                    "jwt_secret_key",
+                    { expiresIn: "1d" }
+                );
+                res.cookie("token", token);
+                return res.json({ loginStatus: true });
+            } else {
+                return res.json({ loginStatus: false, Error: "wrong email or password" });
+            }
+        });
+});
+
 router.get('/logout', (req, res) => {
     res.clearCookie('token')
     return res.json({Status: true})
