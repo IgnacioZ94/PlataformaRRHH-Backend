5ae0296bd98738cb7b55ee1fb747f2bb1d0f8882
flicciardi3112 <francisco.licciardi@gmail.com>
2024-07-03 20:59:10 -0300

    Se agregan multiples endpoints de Admin y se termina login de Employee.

--- Routes/AdminRoute.js
+++ Routes/AdminRoute.js
@@ -27,10 +27,91 @@ router.post("/adminlogin", (req, res) => {
                 res.cookie("token", token);
                 return res.json({ loginStatus: true });
             } else {
-                return res.json({ loginStatus: false, Error: "wrong email or password" });
+                return res.json({ loginStatus: false, Error: "Credenciales incorrectas." });
             }
         });
 });
+
+router.get('/category', (req, res) => {
+    const sqlQuery = "SELECT * FROM category";
+
+    pool.request()
+        .query(sqlQuery, (err, result) => {
+            if (err) return res.json({Status: false, Error: "Query Error" + err});
+            return res.json({Status: true, Result: result.recordset});
+        });
+});
+
+router.post('/add_category', (req, res) => {
+    const sqlQuery = "INSERT INTO category (name) VALUES (@category)";
+
+    pool.request()
+        .input('category', sql.VarChar, req.body.category)
+        .query(sqlQuery, (err, result) => {
+            if (err) return res.json({Status: false, Error: "Query Error" + err});
+            return res.json({Status: true});
+        });
+});
+
+router.get('/category', (req, res) => {
+    const sqlQuery = "SELECT * FROM category";
+
+    pool.request()
+        .query(sqlQuery, (err, result) => {
+            if (err) return res.json({Status: false, Error: "Query Error" + err});
+            return res.json({Status: true, Result: result.recordset});
+        });
+});
+
+router.post('/add_category', (req, res) => {
+    const sqlQuery = "INSERT INTO category (name) VALUES (@category)";
+
+    pool.request()
+        .input('category', sql.VarChar, req.body.category)
+        .query(sqlQuery, (err, result) => {
+            if (err) return res.json({Status: false, Error: "Query Error" + err});
+            return res.json({Status: true});
+        });
+});
+router.get('/admin_count', (req, res) => {
+    const sqlQuery = "SELECT COUNT(id) AS admin FROM admin";
+
+    pool.request()
+        .query(sqlQuery, (err, result) => {
+            if(err) return res.json({Status: false, Error: "Query Error: " + err})
+            return res.json({Status: true, Result: result.recordset})
+        });
+})
+
+router.get('/employee_count', (req, res) => {
+    const sqlQuery = "SELECT COUNT(id) AS employee FROM employee";
+
+    pool.request()
+        .query(sqlQuery, (err, result) => {
+            if(err) return res.json({Status: false, Error: "Query Error: " + err})
+            return res.json({Status: true, Result: result.recordset})
+        });
+})
+
+router.get('/salary_count', (req, res) => {
+    const sqlQuery = "SELECT SUM(salary) AS salaryOFEmp FROM employee";
+
+    pool.request()
+        .query(sqlQuery, (err, result) => {
+            if(err) return res.json({Status: false, Error: "Query Error: " + err})
+            return res.json({Status: true, Result: result.recordset})
+        });
+})
+
+router.get('/admin_records', (req, res) => {
+    const sqlQuery = "SELECT * FROM admin";
+
+    pool.request()
+        .query(sqlQuery, (err, result) => {
+            if(err) return res.json({Status: false, Error: "Query Error: " + err})
+            return res.json({Status: true, Result: result.recordset})
+        });
+})
 
 router.get('/logout', (req, res) => {
     res.clearCookie('token')
--- Routes/EmployeeRoute.js
+++ Routes/EmployeeRoute.js
@@ -1,9 +1,40 @@
 import express from 'express'
-import con from "../utils/db.js";
+import pool from "../utils/db.js";
 import jwt from "jsonwebtoken";
 import bcrypt from 'bcrypt'
+import sql from 'mssql';
 
 const router = express.Router()
+
+router.post("/employee_login", (req, res) => {
+  const sqlQuery = "SELECT * from employee Where email = @email";
+
+  pool.request()
+      .input('email', sql.VarChar, req.body.email)
+      .query(sqlQuery, (err, result) => {
+        if (err) return res.json({ loginStatus: false, Error: "Query error" + err });
+        if (result.recordset.length > 0) {
+          bcrypt.compare(req.body.password, result[0].password, (err, response) => {
+              if (err) return res.json({ loginStatus: false, Error: "Wrong Password" });
+              if(response) {
+                  const email = result[0].email;
+                  const token = jwt.sign(
+                      { role: "employee", email: email, id: result[0].id },
+                      "jwt_secret_key",
+                      { expiresIn: "1d" }
+                  );
+                  res.cookie('token', token)
+                  return res.json({ loginStatus: true, id: result[0].id });
+              }
+          })
+
+        } 
+        else 
+        {
+            return res.json({ loginStatus: false, Error:"Credenciales incorrectas." });
+        }
+      });
+});
 
 router.get('/logout', (req, res) => {
   res.clearCookie('token')
